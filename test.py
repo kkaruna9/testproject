f37eb178ebd29ebef4d86a7887053a48d9c94c77
Krishna moorthy <69917284+kkaruna9@users.noreply.github.com>
2022-11-04 22:16:18 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+api_url ="https://testholder.com"
+response = request.get(f"{api_url}/(num)
+print(response.json())
+
+def findthemutiple(num)
+	if num%3==0 and num%5==0:
+		return 'GN'
+	
+	elif num %3 ==0:
+	    return 'G'
+		
+	elif num %5 ==0:
+		return 'N'
+	else:
+		return num 
+		
+if __name__=="__main__":
+ 	findthemutiple(num)	
